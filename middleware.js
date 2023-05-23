ba2c168e55d2fe84e330f5d4009760cd2bdbe87b
Cfcool100 <90472774+Cfcool100@users.noreply.github.com>
2023-05-23 23:13:28 +0000

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,7 +4,8 @@ const AppAvailability = (req, res, next) => {
     const currentDay = new Date().getDay();
     const currentHours = new Date().getHours();
 
-    if ((currentDay >= 3 && currentDay <= 5) && (currentHours >= 9 && currentHours <= 17)) {
+    if ((currentDay >= 1 && currentDay <= 5) && (currentHours >= 9 && currentHours <= 17)) {
+        console.log(`il est ${currentDay}h `);
         next();
     }else{
         res.status(403).send(`
